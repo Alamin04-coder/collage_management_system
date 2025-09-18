b59b8c64f5e39918fb97dee95f9ab9075c8d16ab
Alamin04-coder <hridoy01004@gmail.com>
2025-09-18 16:54:47 +0600

    Update app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,8 +12,8 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->alias([
-            'role' =>App\http\Middleware\userRole::class,
-            'redirect.role'=>App\http\Middleware\RedirectRoleBase::class,
+            'role' =>App\Http\Middleware\userRole::class,
+            'redirect.role'=>App\Http\Middleware\RedirectRoleBase::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
